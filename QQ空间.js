df7194905373b4d981d564543e347d6deb7cdcfa
HandsomeBarBoy <34203950+HandsomeBarBoy@users.noreply.github.com>
2019-01-31 15:01:21 +0800

    Add files via upload

---
+++ QQ空间.js
@@ -0,0 +1,23 @@
+function cc(){
+    var c = document.getElementById("user");
+    c.value = "";
+}
+function tt(){
+    var t = document.getElementById("user").value;
+    var a = document.getElementById("clear-user");
+    if(t.length==0){
+        a.style.display="none";
+    }else{
+        a.style.display="inline-block";
+    }
+}
+setInterval(tt,10);
+function tt1(){
+    var t = document.getElementById("password").value;
+    var a = document.getElementById("clear-password");
+    if(t.value==" "){
+        a.style.display="none";
+    }else{
+        a.style.display="inline-block";
+    }
+}
